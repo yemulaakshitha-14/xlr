1a456e7e8336b18d1a21d5b2b34697591fb7da20
Akshitha_yemula <akshithayemula1416@gmail.com>
2025-01-03 10:11:48 +0530

    Add files via upload

---
+++ class Demo6.java
@@ -0,0 +1,12 @@
+class Demo6
+{
+    public static void main(String[] args) {
+        String username="yemula_akshitha";
+        String password="akki_123";
+        while (username == "yemula_akshitha" && password == "akki_123"){
+            System.out.println("WELCOME,AKSHITHA");
+            break;
+        }
+        
+    }
+}
