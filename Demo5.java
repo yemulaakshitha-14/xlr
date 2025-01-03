6e9f4d06a67f0274e42c6e127bc451bc6e88d5af
Akshitha_yemula <akshithayemula1416@gmail.com>
2025-01-03 09:52:35 +0530

    Add files via upload
    username and password

---
+++ Demo5.java
@@ -0,0 +1,12 @@
+class Demo5
+{
+    public static void main(String[] args) {
+        String username="user001";
+        String password="user_001";
+        while (username == "user001" && password == "user_001") {
+            System.out.println("WELCOME,HAPPY NEW YEAR");
+            break;
+        }
+        
+    }
+}
