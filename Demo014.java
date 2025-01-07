39372524d4fcc7ebdfc8a2a88607cc1242e4e946
Akshitha_yemula <akshithayemula1416@gmail.com>
2025-01-07 10:12:52 +0530

    Add files via upload
    unary operators

---
+++ Demo014.java
@@ -0,0 +1,22 @@
+public class Demo014 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = -5;
+        boolean isTrue = true;
+
+        
+        System.out.println("Unary Plus: " + (+a)); 
+
+        
+        System.out.println("Unary Minus: " + (-b)); 
+
+        
+        System.out.println("Increment: " + (a++));
+        System.out.println("After Increment: " + a); 
+        
+        System.out.println("Decrement: " + (--b)); 
+
+        
+        System.out.println("Logical NOT: " + (!isTrue)); 
+}
+}
