f0a8159e48469cef9f9447fd3930b1898913db22
Akshitha_yemula <akshithayemula1416@gmail.com>
2025-01-25 20:17:51 +0530

    Add files via upload

---
+++ test002.java
@@ -0,0 +1,9 @@
+public class test002 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i != 98) {
+                System.out.println(i);
+            }
+        }
+    }
+}
