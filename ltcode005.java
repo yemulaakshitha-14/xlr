6f7cd261b9d8d97bcbe59fe1da7a3e4496095c32
Akshitha_yemula <akshithayemula1416@gmail.com>
2025-01-25 20:30:57 +0530

    Add files via upload

---
+++ ltcode005.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ltcode005 {
+    
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];  
+            
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = twoSum(nums1, target1);
+        System.out.println("Indices for nums1: [" + result1[0] + ", " + result1[1] + "]");
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = twoSum(nums2, target2);
+        System.out.println("Indices for nums2: [" + result2[0] + ", " + result2[1] + "]");
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = twoSum(nums3, target3);
+        System.out.println("Indices for nums3: [" + result3[0] + ", " + result3[1] + "]");
+    }
+}
